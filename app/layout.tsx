1b926ceaeddd62831eac52f8e90e355a26aeb26e
rostyslav48 <rostyslavkravchuk05@gmail.com>
2023-11-08 18:33:24 +0200

    create layour and filestructure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
+import type { Metadata } from "next";
+
+import { inter } from "@/app/ui/fonts";
+import "@/app/ui/global.css";
+
+export const metadata: Metadata = {
+  title: "BusinessStore",
+  description: "A store where you can find your business",
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
